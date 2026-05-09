dd57368a5df8aa1bcf91c87afedc458da4f0bfb8
Abhay Pratap Singh <145631801+ard3522o@users.noreply.github.com>
2026-05-09 13:00:30 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1;
+      while(n > 0){
+ans *= x ;
+n--;
+      } 
+      while(n < 0){
+        ans /= x;
+        n++;
+      } 
+      return ans;
+
+    }
+}
