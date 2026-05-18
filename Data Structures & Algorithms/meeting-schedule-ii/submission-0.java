3845f2698e6d8249a2696f09077562a213dd7d05
Abhay Pratap Singh <145631801+ard3522o@users.noreply.github.com>
2026-05-18 16:46:30 +0530

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.java
@@ -0,0 +1,22 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+int rooms = 1;
+for(int i=1;i<intervals.size();i++){
+    if(intervals.get(i).start < intervals.get(i-1).end){
+rooms += 1;
+    }
+}
+return rooms;
+    }
+}
