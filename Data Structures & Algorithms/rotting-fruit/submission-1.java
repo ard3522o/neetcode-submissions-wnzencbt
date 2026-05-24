2e88a6b65fc2ab014c7e7565fa3d9b1c849dd60a
Abhay Pratap Singh <145631801+ard3522o@users.noreply.github.com>
2026-05-24 10:04:07 +0530

    Add: rotting-fruit - submission-1

---
+++ Data Structures & Algorithms/rotting-fruit/submission-1.java
@@ -0,0 +1,52 @@
+class Solution {
+    int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    Queue<int[]> q = new LinkedList<>();
+    int time = 0;
+    int fresh = 0;
+    boolean[][] vis;
+    int n, m;
+    public int orangesRotting(int[][] grid) {
+         n= grid.length;
+         m = grid[0].length;
+        vis = new boolean[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 1)fresh++;
+                else if(grid[i][j] == 2){ 
+                
+                    vis[i][j] =true; 
+                    q.offer(new int[]{i, j});
+                    }
+
+            }
+        }
+        return bfs(grid);
+    }
+int bfs(int[][] grid){
+    while(!q.isEmpty()){
+        int size = q.size();
+        for(int i=0;i<size;i++){
+            int[] arr = q.poll();
+            
+            int rw = arr[0];
+            int cw = arr[1];
+            vis[rw][cw] = true;
+            for(int[] dir : directions){
+                int nr = rw + dir[0];
+                int nc = cw + dir[1];
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m && !vis[nr][nc] && grid[nr][nc] == 1){
+grid[nr][nc] = 2;
+fresh--;
+vis[nr][nc] = true;
+q.offer(new int[]{nr, nc});
+                }
+                
+            }
+            
+        }
+        time++;
+        
+    }
+    return fresh==0?time-1:-1;
+}
+}
