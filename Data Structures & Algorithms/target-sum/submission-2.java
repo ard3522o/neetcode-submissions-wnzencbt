4126128b086d9d59fee71dad0a230da87b381de8
Abhay Pratap Singh <145631801+ard3522o@users.noreply.github.com>
2026-06-14 16:06:39 +0530

    Add: target-sum - submission-2

---
+++ Data Structures & Algorithms/target-sum/submission-2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+   int n = nums.length;
+   int sum =0;
+   for(int i=0;i<n;i++){
+sum += nums[i];
+   }  
+      if(Math.abs(target) > sum || (target + sum)%2 != 0)return 0;
+
+      int s1 = (target + sum)/2;
+      int[][] dp = new int[n+1][s1+1];
+      for(int i=0;i<=n;i++){
+dp[i][0] = 1;
+      }
+     for(int i=1;i<=n;i++){
+        for(int j=0;j<=s1;j++){
+            int take = 0;
+            if(nums[i-1] <= j){
+take = dp[i][j-nums[i-1]];
+            }
+            int notake = dp[i-1][j];
+            dp[i-1][j] = take + notake ;
+        }
+     }
+     return  dp[n][s1];
+    }
+}
