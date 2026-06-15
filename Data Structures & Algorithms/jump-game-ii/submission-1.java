4346897ac3122a2954879831e8b31c4e8ee3f83d
Abhay Pratap Singh <145631801+ard3522o@users.noreply.github.com>
2026-06-15 09:07:48 +0530

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int jump(int[] nums) {
+      int curr =0;
+      int far = 0;
+      int jumps = 0;
+      int n = nums.length;
+      if(n <= 1) return 0;
+      for(int i=0;i<n-1;i++){
+        far = Math.max(far, i+nums[i]);
+
+if(i == curr){
+    jumps++;
+    far = curr;
+}
+if(curr >= n-1){
+    break;
+}
+
+      }  
+      return jumps+1;
+    }
+}
